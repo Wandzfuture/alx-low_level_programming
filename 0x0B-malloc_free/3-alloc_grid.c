c24bac95d6b7a05c1021df4d068f6e58838c1269
Wandzfuture <michael.zabas@gmail.com>
2023-10-31 12:08:05 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (len <= 0)
 		return (NULL);
 
-	array = (int **) malloc(sizeof(int*) * height);
+	array = (int **) malloc(sizeof(int *) * height);
 	if (array == NULL)
 		return (NULL);
 
