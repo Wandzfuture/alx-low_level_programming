c7dcd878108494f4247c2dae0176a3ba41114b02
Wandzfuture <michael.zabas@gmail.com>
2023-11-23 09:25:24 -0800

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,10 +17,10 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i]<'0' || b[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		uint_dec = (uint_dec << 1) + (b[i] - '0');
 	}
-	
+
 	return (uint_dec);
 }
