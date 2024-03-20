3515604901f8fe1dd269c6ced00c9e993ce6aa25
Wandzfuture <michael.zabas@gmail.com>
2024-03-20 10:23:08 -0700

    0x1C. C - Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a function to calculate the perimeter of an island."""
+
+def island_perimeter(grid):
+    """Calculate the perimeter of an island defined by a grid.
+
+    The grid represents an island where water is denoted by 0 and land by 1.
+
+    Args:
+        grid (list): A 2D list of integers representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
